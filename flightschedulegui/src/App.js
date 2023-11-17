b0e75d442336584798aa3d05cc4697d3e7e2b5e3
Alexander Kimani Lhadj <alex.kimani.tic@gmail.com>
2023-11-18 00:25:00 +0100

    Tweaks in table structure for better representation

--- flightschedulegui/src/App.js
+++ flightschedulegui/src/App.js
@@ -1,19 +1,24 @@
-// src/App.js
 import React from 'react';
 import ArrivalList from './components/ArrivalList';
 import DepartureList from './components/DepartureList';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 
 const App = () => {
   return (
     <div style={{ backgroundColor: '#add8e6', minHeight: '100vh', padding: '20px' }}>
       <div className="container mt-5 text-center" style={{ color: '#001f3f' }}>
         <h1 className="display-4 mb-4">OPERATIONS | Flight Schedule APP</h1>
-        <ArrivalList />
-        <DepartureList />
+        <div className="row">
+          <div className="col-md-6">
+            <ArrivalList />
+          </div>
+          <div className="col-md-6">
+            <DepartureList />
+          </div>
+        </div>
       </div>
     </div>
   );
 }
+
 export default App;
